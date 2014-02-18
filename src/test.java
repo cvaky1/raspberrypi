4e18256c0b960bd62cc66ce24b7226fb638a4974
Cvaky <Cvaky@Cvaky-PC>
2014-02-18 13:22:17 +0100

    a

--- src/test.java
+++ src/test.java
@@ -12,9 +12,9 @@ public class test {
 		Robot robot = new Robot();
 		while (loop) {
 			
-			robot.delay(500);
 			String s = in.nextLine();
 			robot.keyPress(KeyEvent.VK_ENTER);
+			robot.delay(500);
 			if (s != "") {
 				System.out.println(s);
 			}
