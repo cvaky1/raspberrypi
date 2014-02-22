893f8461189370861b1c673a58d95f9987395466
Cvaky <Cvaky@Cvaky-PC>
2014-02-22 15:51:56 +0100

    s

--- src/nieco.java
+++ src/nieco.java
@@ -1,18 +1,26 @@
 import java.io.Console;
-import java.io.IOException;
-import java.lang.System;
-public class nieco {
-
-	/**
-	 * @param args
-	 * @throws IOException 
-	 * @throws InterruptedException 
-	 */
-	public static void main(String[] args) throws IOException, InterruptedException {
-		// TODO Auto-generated method stub
-
-		String[] cmd = {"/bin/sh", "-c", "stty raw </dev/tty"};
-	    Runtime.getRuntime().exec(cmd).waitFor();
+	import java.io.IOException;
+	import java.io.Reader;
+	import java.util.ArrayList;
+	
+	
+	public class nieco  {
+		
+		public static void main(String[] args) throws IOException, InterruptedException {
+			String[] cmd = {"/bin/sh", "-c", "stty raw </dev/tty"};
+			Runtime.getRuntime().exec(cmd).waitFor();
+			Console console = System.console();
+			Reader reader = console.reader();
+			ArrayList<Long> timeStamps = new ArrayList<Long>();
+			StringBuilder password = new StringBuilder();
+			timeStamps.add(System.currentTimeMillis());
+			System.out.println("Enter your 8 character password");
+			for(int i = 0;i<8;i++) {
+				password.append(reader.read());
+				timeStamps.add(System.currentTimeMillis());
+			}
+			System.out.println(password);
+			cmd = new String[] {"/bin/sh", "-c", "stty sane </dev/tty"};
+			Runtime.getRuntime().exec(cmd).waitFor();			
+		}
 	}
-
-}
